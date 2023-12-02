f0ce19e9bb721b7f5f87a54f012c176be1f4bc14
Renan Fialho <renannascimento@sisconsultoria.com.br>
2023-12-02 06:34:11 -0300

    Refactor: Melhoria do metodos de criar lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,30 @@
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton')
+const maxRecordCount = 11;
 const limit = 5;
 let offset = 0;
 
-function convertPokemonTypesToLi(pokemonsTypes) {
-    return pokemonsTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
-function convertPokemonToLi(pokemon) {
-    return `
-    <li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.id}</span>
-        <span class="name">${pokemon.name}</span>
-        <div class="detail">
-            <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-            </ol>
-            <img src="${pokemon.photo}"
-            alt="${pokemon.name}">
-        </div>
-        
-    </li>`
-}
-
 function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons().then((pokemons = []) => {
-        console.log(pokemonList)
-        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        pokemonList.innerHTML += pokemons.map((pokemon) =>  `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.id}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}"
+                alt="${pokemon.name}">
+            </div>
+            
+        </li>`).join('');
     });
 }
 
-
-pokeApi.getPokemons().then((pokemons = []) => {
-    console.log(pokemonList)
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
-});
+loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
-
+    offset += limit;
+    loadPokemonItens(offset, limit);
 });
